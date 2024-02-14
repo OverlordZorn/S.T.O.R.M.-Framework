a0989cad8c3e130aa4526042f431a3c5ba9269e6
Mr. Zorn <dennisAzorn@gmail.com>
2024-02-14 06:38:44 +0100

    Update CVO_FilmGrain.hpp

--- addons/storm/CVO_FilmGrain.hpp
+++ addons/storm/CVO_FilmGrain.hpp
@@ -2,13 +2,14 @@ class CVO_FG_Default
 {
 	ppEffectType = "FilmGrain";
 	ppEffectPrio = "2000";
+	layer = 0;
 
 	intensity =   0.005;		// 0..1
-	sharpness =   1.25;			// 1..20
-	grainSize =   2.01;			// 1..8
-	intensityX0 = 0.75;			// -x..0..+x
-	intensityX1 = 1.00;			// -x..0..+x
-	monochromatic = 0;			// 0,1 # 0 Monochromatic # Any other value Color
+	sharpness =    1.25;		// 1..20
+	grainSize =    2.01;		// 1..8
+	intensityX0 =  0.75;		// -x..0..+x
+	intensityX1 =  1.00;		// -x..0..+x
+	monochromatic =   0;		// 0,1 # 0 Monochromatic # Any other value Color
 };
 
 class CVO_FG_Basic : CVO_FG_Default
@@ -20,3 +21,14 @@ class CVO_FG_Basic : CVO_FG_Default
 	intensityX1 = 0.1;			// -x..0..+x
 	monochromatic = 0;			// 0,1 # 0 Monochromatic # Any other value Color
 };
+
+class CVO_FG_Storm : CVO_FG_Default
+{
+	intensity =   0.15;			// 0..1
+	sharpness =   0.7;			// 1..20
+	grainSize =   2.4;			// 1..8
+	intensityX0 = 0.2;			// -x..0..+x
+	intensityX1 = 0.5;			// -x..0..+x
+	monochromatic = 0;			// 0,1 # 0 Monochromatic # Any other value Color
+};
+
