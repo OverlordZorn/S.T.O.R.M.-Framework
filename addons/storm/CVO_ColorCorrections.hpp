a7791838ff24fecf88cbeb938fcf1a6777ef90e1
Mr. Zorn <dennisAzorn@gmail.com>
2024-02-14 07:22:46 +0100

    Update CVO_ColorCorrections.hpp

--- addons/storm/CVO_ColorCorrections.hpp
+++ addons/storm/CVO_ColorCorrections.hpp
@@ -11,7 +11,7 @@ class CVO_CC_Default
 	blending_rgba[] = 		{		0,		0,		0,		0	};			// RGB - Color -- A - Blend Factor (0-> Original Color - 1 -> Blended Color)
 	colorization_rgba[] = 	{		1,		1,		1,		1	};			// RGB - Color -- A - Saturation   (0-> Original Color - 1 -> B&W moltiplied by colorize color)
 	desaturation_rgba0[] = 	{	0.299, 	0.587, 	0.114, 		0	};			// RGB - Color -- 0 - not in use? -- color rgb weights for desaturation
-	
+
 	radial_color[] = {
 		-1,												// 0..1			major axis radius of ellipse
 		-1,												// 0..1			minor axis radius of ellipse
@@ -19,8 +19,8 @@ class CVO_CC_Default
 		 0,												// -0.5 .. 0.5	centerX of ellipse on the screen (in relative coords)  where 0 is screen center
 		 0,												// -0.5 .. 0.5	centerY of ellipse on the screen (in relative coords) -0.5 .. 0.5 where 0 is screen center
 		 0,												// 0..1			coefficient for inner radius (where effect is not applied)
-		 0												// 0..			coefficient for color interpolation between inner and outer radius
-	}; 	
+		 1												// 0..			coefficient for color interpolation between inner and outer radius
+	}; 
 };
 
 class CVO_CC_Alias : CVO_CC_Default
@@ -52,14 +52,67 @@ class CVO_CC_Orange_Strong : CVO_CC_Default
 	desaturation_rgba0[] = 	{	 0.33,	 0.33,   0.33,   0.00	};			// RGB - Color -- 0 - not in use? -- color rgb weights for desaturation
 };
 
-class CVO_CC_Test : CVO_CC_Default
+class CVO_CC_Radial_Default
 {
-	layer = 1;
+	ppEffectType = "ColorCorrections";
 	ppEffectPrio = "1600";
-	brightness = 	0.80;
-	contrast = 		0.80;
-	offset = 		0.05;
-	blending_rgba[] = 		{	  0.5,	-0.10, 	-0.60,	 0.20	};			// RGB - Color -- A - Blend Factor (0-> Original Color - 1 -> Blended Color)
-	colorization_rgba[] = 	{	 -0.4,	 1.00,	 2.40,	 1.30	};			// RGB - Color -- A - Saturation   (0-> Original Color - 1 -> B&W moltiplied by colorize color)
-	desaturation_rgba0[] = 	{	 0.33,	 0.33,   0.33,   0.00	};			// RGB - Color -- 0 - not in use? -- color rgb weights for desaturation
+	layer = 1;
+
+	brightness = 1;															
+	contrast = 1;
+	offset = 0;
+	
+	blending_rgba[] = 		{		0,		0,		0,		0	};			// RGB - Color -- A - Blend Factor (0-> Original Color - 1 -> Blended Color)
+	colorization_rgba[] = 	{		1,		1,		1,		1	};			// RGB - Color -- A - Saturation   (0-> Original Color - 1 -> B&W moltiplied by colorize color)
+	desaturation_rgba0[] = 	{	0.299, 	0.587, 	0.114, 		0	};			// RGB - Color -- 0 - not in use? -- color rgb weights for desaturation
+	
+	radial_color[] = {
+		-1,												// 0..1			major axis radius of ellipse
+		-1,												// 0..1			minor axis radius of ellipse
+		 0,												// 0..359		rotation of ellipse axis (in degrees) 
+		 0,												// -0.5 .. 0.5	centerX of ellipse on the screen (in relative coords)  where 0 is screen center
+		 0,												// -0.5 .. 0.5	centerY of ellipse on the screen (in relative coords) -0.5 .. 0.5 where 0 is screen center
+		 0,												// 0..1			coefficient for inner radius (where effect is not applied)
+		 1												// 0..			coefficient for color interpolation between inner and outer radius
+	}; 	
+};
+
+class CVO_CC_Radial_Blinking_open : CVO_CC_Radial_Default
+{
+	brightness = 0.1;
+	radial_color[] = {
+		1,												// 0..1			major axis radius of ellipse
+		1,												// 0..1			minor axis radius of ellipse
+		 0,												// 0..359		rotation of ellipse axis (in degrees) 
+		 0,												// -0.5 .. 0.5	centerX of ellipse on the screen (in relative coords)  where 0 is screen center
+		 0,												// -0.5 .. 0.5	centerY of ellipse on the screen (in relative coords) -0.5 .. 0.5 where 0 is screen center
+		 0,												// 0..1			coefficient for inner radius (where effect is not applied)
+		 1												// 0..			coefficient for color interpolation between inner and outer radius
+	};
+};
+
+class CVO_CC_Radial_Blinking_half : CVO_CC_Radial_Default
+{
+	brightness = 0.1;
+	radial_color[] = {
+		0.5,												// 0..1			major axis radius of ellipse
+		0.5,												// 0..1			minor axis radius of ellipse
+		 0,												// 0..359		rotation of ellipse axis (in degrees) 
+		 0,												// -0.5 .. 0.5	centerX of ellipse on the screen (in relative coords)  where 0 is screen center
+		 0,												// -0.5 .. 0.5	centerY of ellipse on the screen (in relative coords) -0.5 .. 0.5 where 0 is screen center
+		 0,												// 0..1			coefficient for inner radius (where effect is not applied)
+		 1												// 0..			coefficient for color interpolation between inner and outer radius
+	};
+};
+class CVO_CC_Radial_Blinking_closed : CVO_CC_Radial_Default
+{
+	radial_color[] = {
+		0.5,												// 0..1			major axis radius of ellipse
+		0.05,												// 0..1			minor axis radius of ellipse
+		 0,												// 0..359		rotation of ellipse axis (in degrees) 
+		 0,												// -0.5 .. 0.5	centerX of ellipse on the screen (in relative coords)  where 0 is screen center
+		 0,												// -0.5 .. 0.5	centerY of ellipse on the screen (in relative coords) -0.5 .. 0.5 where 0 is screen center
+		 0,												// 0..1			coefficient for inner radius (where effect is not applied)
+		 1												// 0..			coefficient for color interpolation between inner and outer radius
+	};
 };
