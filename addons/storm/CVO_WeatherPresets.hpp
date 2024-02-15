6474cdf4220f9932b2003a8de05568e2ce4a4599
Mr. Zorn <dennisAzorn@gmail.com>
2024-02-15 14:43:57 +0100

    move .hpp -> /cfg/

--- addons/storm/CVO_WeatherPresets.hpp
+++
@@ -1,6 +0,0 @@
-class CVO_Weather_Default
-{
-    rainParams = "CVO_RainParams_Default";
-    overcast = 0;
-    windStr = 0;
-};
---
+++ addons/storm/Cfg/CVO_WeatherPresets.hpp
@@ -0,0 +1,32 @@
+class CVO_Weather_Default
+{
+    change_overcast = 0;
+    overcast = 0;
+
+    change_rainValue = 0;
+    rainValue = 0;
+
+    change_rainParams = 0;
+    rainParams = "CVO_RainParams_Default";
+
+    change_lightnings = 0;
+    lightnings_value = 0;
+
+    change_fog = 0;
+
+    fog_value_min = 0;
+    fog_value_max = 0;
+    fog_density = 0;
+    fog_useAvgASL = 0;
+    fog_base = 0;
+
+    change_windStr = 0;
+    windStr = 0;
+
+    change_gusts = 0;
+    gusts_value = 0;
+
+    change_waves = 0;
+    waves_vale = 0;
+
+};
