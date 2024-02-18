929aea91b6ab07d3f05e8ff8ba925be9b27149fd
Mr. Zorn <dennisAzorn@gmail.com>
2024-02-19 00:01:35 +0100

    Update CVO_WeatherPresets.hpp

--- addons/storm/Cfg/CVO_WeatherPresets.hpp
+++ addons/storm/Cfg/CVO_WeatherPresets.hpp
@@ -1,33 +1,65 @@
 class CVO_Weather_Default
 {
-    change_overcast = 0;
-    overcast_value = 0;
+    change_overcast = 0;                            // 0 or 1 - consider it a bool
+    overcast_value = 0;                             // 0..1
 
-    change_rainValue = 0;
-    rain_Value = 0;
+    change_rainValue = 0;                           // 0 or 1 - consider it a bool
+    rain_Value = 0;                                 // 0..1
 
-    change_rainParams = 0;
-    rainParams = "CVO_RainParams_Default";
+    change_rainParams = 0;                          // 0 or 1 - consider it a bool
+    rainParams = "CVO_RainParams_Default";          // String - name of RainParams Config Class
 
-    change_lightnings = 0;
-    lightnings_value = 0;
+    change_lightnings = 0;                          // 0 or 1 - consider it a bool
+    lightnings_value = 0;                           // 0..1
 
-    change_fog = 0;
+    change_fog = 0;                                 // 0 or 1 - consider it a bool
 
-    fog_value_min = 0;
-    fog_value_max = 0;
-    fog_density = 0;
-    fog_base = 0;
-    fog_use_AvgASL = 0;
-    fog_use_AvgASL_continous = 0;
+    fog_value_min = 0;                              // 0..1   - Minimum Fog, even with 1% intensity                              
+    fog_value_max = 0;                              // 0..1   - Maximum Fog Level at 100% intensity
+    fog_dispersion = 0;                             // 0..1   - Recommend to stay within 0 .. 0.1
+    fog_base = 0;                                   // number - meters +/- above Sea Level
+    fog_use_AvgASL = 0;                             // 0 or 1 - consider it a bool
+    fog_use_AvgASL_continous = 0;                   // 0 or 1 - consider it a bool
 
-    change_wind = 0;
-    wind_value = 0;
+    change_wind = 0;                                // 0 or 1 - consider it a bool
+    wind_value = 0;                                 // Number - 0.. alot, eventhough +200 values getting cray cray
 
-    change_gusts = 0;
-    gusts_value = 0;
+    change_gusts = 0;                               // 0 or 1 - consider it a bool
+    gusts_value = 0;                                // 0..1   - wind Gusts, changes in windspeed
 
-    change_waves = 0;
-    waves_value = 0;
+    change_waves = 0;                               // 0 or 1 - consider it a bool
+    waves_value = 0;                                // 0..1
+};
 
+class CVO_Weather_Sandstorm_01 : CVO_Weather_Default
+{
+    change_overcast = 1;                            // 0 or 1 - consider it a bool
+    overcast_value = 1;                             // 0..1
+
+    change_rainValue = 1;                           // 0 or 1 - consider it a bool
+    rain_Value = 0;                                 // 0..1
+
+    change_rainParams = 0;                          // 0 or 1 - consider it a bool
+    rainParams = "CVO_RainParams_Default";          // String - name of RainParams Config Class
+
+    change_lightnings = 1;                          // 0 or 1 - consider it a bool
+    lightnings_value = 0;                           // 0..1
+
+    change_fog = 1;                                 // 0 or 1 - consider it a bool
+
+    fog_value_min = 0.1;                            // 0..1   - Minimum Fog, even with 1% intensity                              
+    fog_value_max = 0.5;                            // 0..1   - Maximum Fog Level at 100% intensity
+    fog_dispersion = 0.025;                         // 0..1   - Recommend to stay within 0 .. 0.1
+    fog_base = 50;                                  // number - meters +/- above Sea Level
+    fog_use_AvgASL = 0;                             // 0 or 1 - consider it a bool
+    fog_use_AvgASL_continous = 1;                   // 0 or 1 - consider it a bool
+
+    change_wind = 1;                                // 0 or 1 - consider it a bool
+    wind_value = 75;                                // Number - 0.. alot, eventhough +200 values getting cray cray
+
+    change_gusts = 1;                               // 0 or 1 - consider it a bool
+    gusts_value = 1;                                // 0..1   - wind Gusts, changes in windspeed
+
+    change_waves = 1;                               // 0 or 1 - consider it a bool
+    waves_value = 1;                                // 0..1
 };
