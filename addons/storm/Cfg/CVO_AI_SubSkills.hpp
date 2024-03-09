3db28cc2352a6baa7bb633d8cb8ae3d9a0871a49
Mr. Zorn <dennisAzorn@gmail.com>
2024-03-09 07:49:46 +0100

    fixed property names

--- addons/storm/Cfg/CVO_AI_SubSkills.hpp
+++ addons/storm/Cfg/CVO_AI_SubSkills.hpp
@@ -1,3 +1,19 @@
-class CVO_SubSkills
+class CVO_AI_SubSkill_Modifier
 {
+    class CVO_AI_Skill_sandstorm_old
+    {
+        general =           0.40;
+        courage =           0.30;
+        aimingAccuracy =    0.15;
+        aimingShake =       0.50;
+        aimingSpeed =       0.40;
+        commanding =        0.40;
+        spotDistance =      0.10;
+        spotTime =          0.10;
+        reloadSpeed =       0.50;
+
+//      endurance =     0.80;     // doesnt exist in arma
+    };
 };
+
+
