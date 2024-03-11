25c1e2ed0ffe034e81752adcf8e39a922163ec57
Mr. Zorn <dennisAzorn@gmail.com>
2024-03-11 06:43:54 +0100

    Update CVO_StormPresets.hpp

--- addons/storm/Cfg/CVO_StormPresets.hpp
+++ addons/storm/Cfg/CVO_StormPresets.hpp
@@ -4,10 +4,17 @@ class CVO_StormPresets
     {
         storm_name = "CVO_Storm_Default";
 
-        weather_preset = "CVO_Weather_Default";
-        simple_pp_effects[] = { "CVO_CC_Default", "CVO_FG_Default", "CVO_DB_Default" };
-        particle_effects[] = {    };
-        subSkill_preset = "";
+        weather_preset = "";
+        weather_coef = 1;
+
+        pp_effects[] = { "", "", "" };
+        pp_effects_coef = 1;
+
+        particle_effects[] = {""};
+        particle_effects_coef = 1;
+        
+        ai_skill_preset = "";
+        ai_skill_coef = 1;
 
     };
 };
