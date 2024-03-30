2a45c25c14bda6276a57bf194e84767309def4a1
Mr. Zorn <dennisAzorn@gmail.com>
2024-03-30 01:36:28 +0100

    fixed property names

--- addons/framework/Storm_MainPresets.inc.hpp
+++ addons/framework/Storm_MainPresets.inc.hpp
@@ -8,24 +8,21 @@ class PVAR(mainPresets)
         mod_skill_preset = ""; // QEGVAR(mod_skill,sandstorm_old)
         mod_skill_coef = 1;
 
-
         fx_weather_preset = "";
-        weather_coef = 1;
+        fx_weather_coef = 1;
 
+        fx_post_presets[] = { "", "", "" };
+        fx_post_coef = 1;
 
-        fx_post[] = { "", "", "" };
-        pp_effects_coef = 1;
-
-        fx_particle[] = {""};
+        fx_particle_presets[] = {""};
         fx_particle_coef = 1;
         
-        fx_sound[] = {""};
+        fx_sound_presets[] = {""};
         fx_sound_coef = 1;
     };
 
     class PVAR(Sandstorm) : PVAR(Default)
     {
-
         storm_name = QPVAR(Sandstorm);
 
         mod_skill_preset = QEGVAR(mod_skill,sandstorm_old);
@@ -34,8 +31,8 @@ class PVAR(mainPresets)
         fx_weather_preset = QEGVAR(fx_weather,Sandstorm_01);
         fx_weather_coef = 1;
 
-        fx_post_presets[] = { QEGVAR(fx_post,CC_Mars_Storm), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20) };
-        pp_effects_coef = 1;
+        fx_post_presets[] = {QEGVAR(fx_post,CC_Mars_Storm), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};
+        fx_post_coef = 1;
 
         fx_particle_presets[] = {QEGVAR(fx_particle,Branches), QEGVAR(fx_particle,Dust_High_35), QEGVAR(fx_particle,Dust_High_100)};
         fx_particle_coef = 1;
@@ -44,5 +41,3 @@ class PVAR(mainPresets)
         fx_sound_coef = 1;
     };
 };
-
-
