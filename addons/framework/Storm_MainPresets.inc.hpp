d5293eb4d1cacb4098db7db9f28242efd3530fb4
Mr. Zorn <dennisAzorn@gmail.com>
2024-04-06 10:14:19 +0200

    starting test of snowstorm parameters

--- addons/framework/Storm_MainPresets.inc.hpp
+++ addons/framework/Storm_MainPresets.inc.hpp
@@ -28,7 +28,7 @@ class PVAR(mainPresets)
         mod_skill_preset = QEGVAR(mod_skill,sandstorm_old);
         mod_skill_coef = 1;
 
-        fx_weather_preset = QEGVAR(fx_weather,Sandstorm_01);
+        fx_weather_preset = QEGVAR(fx_weather,Sandstorm);
         fx_weather_coef = 1;
 
         fx_post_presets[] = {QEGVAR(fx_post,CC_Mars_Storm), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};
@@ -40,4 +40,25 @@ class PVAR(mainPresets)
         fx_sound_presets[] = {QEGVAR(fx_sound,3D_WindLong),QEGVAR(fx_sound,3D_WindBursts)};
         fx_sound_coef = 1;
     };
+
+        class PVAR(SnowStorm) : PVAR(Default)
+    {
+        storm_name = QPVAR(Sandstorm);
+
+        mod_skill_preset = QEGVAR(mod_skill,sandstorm_old);
+        mod_skill_coef = 1;
+
+        fx_weather_preset = QEGVAR(fx_weather,SnowStorm);                                            // Need to make SnowStorm Weather
+        fx_weather_coef = 1;
+
+        fx_post_presets[] = {QEGVAR(fx_post,CC_Muted_Pale_Blue), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};                           // need to get right CC filter // QEGVAR(fx_post,CC_Muted_Pale_Blue)
+        fx_post_coef = 1;
+
+        fx_particle_presets[] = {QEGVAR(fx_particle,Dust_High_100)};                                    // Removed QEGVAR(fx_particle,Branches), QEGVAR(fx_particle,Dust_High_35),
+        fx_particle_coef = 0.5;
+        
+        fx_sound_presets[] = {QEGVAR(fx_sound,3D_WindLong),QEGVAR(fx_sound,3D_WindBursts)};
+        fx_sound_coef = 1;
+    };
+
 };
