ccea0dfff3e6db6330bf07b4890bc8d199cb00b8
Mr. Zorn <dennisAzorn@gmail.com>
2024-04-10 10:28:10 +0200

    Update Storm_MainPresets.inc.hpp

--- addons/framework/Storm_MainPresets.inc.hpp
+++ addons/framework/Storm_MainPresets.inc.hpp
@@ -40,6 +40,21 @@ class PVAR(mainPresets)
         fx_sound_coef = 1;
     };
 
+    class PVAR(Sandstorm_Light) : PVAR(Sandstorm)
+    {
+        fx_post_presets[] = {QEGVAR(fx_post,CC_01), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};
+
+    };
+
+// DUST STORM
+
+    class PVAR(Duststorm_Green) : PVAR(Sandstorm)
+    {
+        fx_post_presets[] = {QEGVAR(fx_post,CC_Alias), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};
+    };
+
+
+
 // SNOW STORMS
 
     class PVAR(SnowStorm) : PVAR(Default)
@@ -47,13 +62,17 @@ class PVAR(mainPresets)
         mod_skill_preset = QEGVAR(mod_skill,reducedVisibility);
         mod_skill_coef = 1;
 
-        fx_weather_preset = QEGVAR(fx_weather,SnowStorm);                                            // Need to make SnowStorm Weather
+        fx_weather_preset = QEGVAR(fx_weather,SnowStorm);
         fx_weather_coef = 1;
 
-        fx_post_presets[] = {QEGVAR(fx_post,CC_ColdSnow), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};                           // need to get right CC filter // QEGVAR(fx_post,CC_Muted_Pale_Blue)
+        fx_post_presets[] = {QEGVAR(fx_post,CC_ColdSnow), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};
         fx_post_coef = 1;
 
+<<<<<<< Updated upstream
         fx_particle_presets[] = {QEGVAR(fx_particle,Snow), QEGVAR(fx_particle,Snow_25)};                                    // Removed QEGVAR(fx_particle,Branches), QEGVAR(fx_particle,Dust_35),
+=======
+        fx_particle_presets[] = {QEGVAR(fx_particle,Branches), QEGVAR(fx_particle,Snow)};               /// , QEGVAR(fx_particle,Snow_25)
+>>>>>>> Stashed changes
         fx_particle_coef = 1;
         
         fx_sound_presets[] = {QEGVAR(fx_sound,3D_WindLong),QEGVAR(fx_sound,3D_WindBursts)};
@@ -63,22 +82,18 @@ class PVAR(mainPresets)
 
     class PVAR(SnowStorm_Bleak) : PVAR(SnowStorm)
     {
-        fx_post_presets[] = {QEGVAR(fx_post,CC_ColdSnow_Bleak), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};                           // need to get right CC filter // QEGVAR(fx_post,CC_Muted_Pale_Blue)
+        fx_post_presets[] = {QEGVAR(fx_post,CC_ColdSnow_Bleak), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};
     };
-
 
     class PVAR(SnowStorm_Calm) : PVAR(SnowStorm)
     {
-        fx_weather_preset = QEGVAR(fx_weather,SnowStorm_Calm);                                            // Need to make SnowStorm Weather
+        fx_weather_preset = QEGVAR(fx_weather,SnowStorm_Calm);
         fx_weather_coef = 1;
-
-        fx_post_presets[] = {QEGVAR(fx_post,CC_ColdSnow), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};                           // need to get right CC filter // QEGVAR(fx_post,CC_Muted_Pale_Blue)
-        fx_post_coef = 1;
-
-        fx_particle_presets[] = {QEGVAR(fx_particle,Snow), QEGVAR(fx_particle,Snow_25)};                                    // Removed QEGVAR(fx_particle,Branches), QEGVAR(fx_particle,Dust_35),
-        fx_particle_coef = 1;
-        
-        fx_sound_presets[] = {QEGVAR(fx_sound,3D_WindLong),QEGVAR(fx_sound,3D_WindBursts)};
         fx_sound_coef = 0.3;
+    };
+
+    class PVAR(SnowStorm_Calm_Bleak) : PVAR(SnowStorm_Calm)
+    {
+        fx_post_presets[] = {QEGVAR(fx_post,CC_ColdSnow_Bleak), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};        
     };
 };
