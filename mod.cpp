906b8f5a1d55e8f61b430b56e8c8352b9285d091
Mr. Zorn <dennisAzorn@gmail.com>
2024-04-18 02:06:03 +0200

    removed script-component
    resolves #54

--- mod.cpp
+++ mod.cpp
@@ -1,8 +1,6 @@
-#include "\z\storm\addons\based\script_macros.hpp"
+name = "S.T.O.R.M. Framework";
 
-name = MOD_NAME_BEAUTIFIED;
-
-author = AUTHOR;
+author = "OverlordZorn [CVO]";
 
 picture         = "img\Title Tower Bleak.paa";                                                                                               // Picture displayed from the expansions menu. Optimal size is 2048x1024
 hideName        = "false";                                                                                                                          // Hide the extension name
@@ -12,15 +10,15 @@ logoSmall       = "img\logo_64.paa";                                            
 logo            = "img\logo_128.paa";                                                                                                               // Logo displayed in the main menu 128x128
 logoOver        = "img\logo_128_contrast.paa";                                                                                                      // When the mouse is over, in the main menu 128x128
 
-tooltip         = MOD_NAME_BEAUTIFIED;
-tooltipOwned    = MOD_NAME_BEAUTIFIED;                                                                                                              // Tool tip displayed when the mouse is left over, in the main menu
-overview        = MOD_NAME_BEAUTIFIED;                                                                                                              // Overview text, displayed from the extension menu
+tooltip         = "S.T.O.R.M. Frameworks";
+tooltipOwned    = "S.T.O.R.M. Framework";                                                                                                              // Tool tip displayed when the mouse is left over, in the main menu
+overview        = "S.T.O.R.M. Framework";                                                                                                              // Overview text, displayed from the extension menu
 description     = "Framework for different types of Weather Storms, for example Sandstorms!";                                                       // Probably in context with action
 action          = "https://github.com/OverlordZorn/S.T.O.R.M.-Framework";                                                                                                                              // Website URL, that can accessed from the expansions menu 
 actionName      = "Github";                                                                                                                         // label of button/tooltip in extension menu
 
 overviewPicture = "img\logo_storm_framework_512.paa";
-overviewText = MOD_NAME_BEAUTIFIED;
+overviewText = "S.T.O.R.M. Framework";
 
 overviewFootnote = "Storms, Technically Optimised, Really Mom!";
 
