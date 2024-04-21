c5a7bb0888497d8a1a1b941520bd0aa4f6f2afc9
Mr. Zorn <dennisAzorn@gmail.com>
2024-04-21 14:11:45 +0200

    fog_dispersion -> fog_decay

--- addons/fx_weather/storm_WeatherPresets.inc.hpp
+++ addons/fx_weather/storm_WeatherPresets.inc.hpp
@@ -19,7 +19,7 @@ class GVAR(Presets)
 
         fog_value_min = 0;                              // 0..1   - Minimum Fog, even with 1% intensity                              
         fog_value_max = 0;                              // 0..1   - Maximum Fog Level at 100% intensity
-        fog_dispersion = 0;                             // 0..1   - Recommend to stay within 0 .. 0.1
+        fog_decay = 0;                                  // 0..1   - Recommend to stay within 0 .. 0.1 - Additional Info: fogDecay - how much the fog density decays with altitude. 0 = constant density, 0.0049333 = density halves every 500m
         fog_base = 0;                                   // number - meters +/- above Sea Level
         fog_mode = 0;                                   // Fogmode: 0 - apply setFog with param once, nothing else fancy going on.  | 1 - Gets Players Average ASL once and adds that to the fog_base Value. | 2 - Continously adapts fogbase based on player AvgAVL.
         fog_boost = 0;
@@ -55,7 +55,7 @@ class GVAR(Presets)
 
         fog_value_min = 0;                              // 0..1   - Minimum Fog, even with 1% intensity                              
         fog_value_max = 0;                              // 0..1   - Maximum Fog Level at 100% intensity
-        fog_dispersion = 0;                             // 0..1   - Recommend to stay within 0 .. 0.1
+        fog_decay = 0;                                  // 0..1   - Recommend to stay within 0 .. 0.1 - Additional Info: fogDecay - how much the fog density decays with altitude. 0 = constant density, 0.0049333 = density halves every 500m
         fog_base = 0;                                   // number - meters +/- above Sea Level
         fog_mode = 0;                                   // Fogmode: 0 - apply setFog with param once, nothing else fancy going on.  | 1 - Gets Players Average ASL once and adds that to the fog_base Value. | 2 - Continously adapts fogbase based on player AvgAVL.
         fog_boost = 0;
@@ -89,9 +89,9 @@ class GVAR(Presets)
 
         fog_value_min = 0.01;                            // 0..1   - Minimum Fog, even with 1% intensity                              
         fog_value_max = 0.4;                            // 0..1   - Maximum Fog Level at 100% intensity
-        fog_dispersion = 0.013;                         // 0..1   - Recommend to stay within 0 .. 0.1
+        fog_decay = 0.013;                              // 0..1   - Recommend to stay within 0 .. 0.1 - Additional Info: fogDecay - how much the fog density decays with altitude. 0 = constant density, 0.0049333 = density halves every 500m
         fog_base = 180;                                 // number - meters +/- above Sea Level
-        fog_mode = 2;                                   // Fogmode: 0 - apply setFog with param once, nothing else fancy going on.  | 1 - Gets Players Average ASL once and adds that to the fog_base Value. | 2 - Continously adapts fogbase based on player AvgAVL.
+        fog_mode = 2;                                   // Fogmode: 0 - apply setFog with param once, nothing else fancy going on - fogDecay of 0 recommended! | 1 - Gets Players Average ASL once and adds that to the fog_base Value. | 2 - Continously adapts fogbase based on player AvgAVL.
         fog_boost = 1;
 
         change_wind = 1;                                // 0 or 1 - consider it a bool
@@ -124,12 +124,14 @@ class GVAR(Presets)
 
         change_fog = 1;                                 // 0 or 1 - consider it a bool
 
-        fog_value_min = 0.01;                            // 0..1   - Minimum Fog, even with 1% intensity                              
+        fog_value_min = 0.01;                           // 0..1   - Minimum Fog, even with 1% intensity                              
         fog_value_max = 0.4;                            // 0..1   - Maximum Fog Level at 100% intensity
-        fog_dispersion = 0.013;                         // 0..1   - Recommend to stay within 0 .. 0.1
+        fog_decay = 0.013;                              // 0..1   - Recommend to stay within 0 .. 0.1 - Additional Info: fogDecay - how much the fog density decays with altitude. 0 = constant density, 0.0049333 = density halves every 500m
         fog_base = 180;                                 // number - meters +/- above Sea Level
         fog_mode = 2;                                   // Fogmode: 0 - apply setFog with param once, nothing else fancy going on.  | 1 - Gets Players Average ASL once and adds that to the fog_base Value. | 2 - Continously adapts fogbase based on player AvgAVL.
         fog_boost = 1;
+
+
 
         change_wind = 1;                                // 0 or 1 - consider it a bool
         wind_value = 7.5;                                // Number - 0.. - ~32m/s is classified as hurricane - see "beaufort wind scale" for reference
@@ -149,7 +151,7 @@ class GVAR(Presets)
     {
         fog_value_min = 0.01;                           // 0..1   - Minimum Fog, even with 1% intensity                              
         fog_value_max = 0.2;                            // 0..1   - Maximum Fog Level at 100% intensity
-        fog_dispersion = 0.013;                         // 0..1   - Recommend to stay within 0 .. 0.1
+        fog_decay = 0.013;                              // 0..1   - Recommend to stay within 0 .. 0.1 - Additional Info: fogDecay - how much the fog density decays with altitude. 0 = constant density, 0.0049333 = density halves every 500m
         fog_base = 180;                                 // number - meters +/- above Sea Level
         fog_mode = 2;                                   // Fogmode: 0 - apply setFog with param once, nothing else fancy going on.  | 1 - Gets Players Average ASL once and adds that to the fog_base Value. | 2 - Continously adapts fogbase based on player AvgAVL.
         fog_boost = 1;
@@ -184,7 +186,7 @@ class GVAR(Presets)
 
         fog_value_min = 0.01;                            // 0..1   - Minimum Fog, even with 1% intensity                              
         fog_value_max = 0.4;                            // 0..1   - Maximum Fog Level at 100% intensity
-        fog_dispersion = 0.015;                         // 0..1   - Recommend to stay within 0 .. 0.1
+        fog_decay = 0.015;                              // 0..1   - Recommend to stay within 0 .. 0.1 - Additional Info: fogDecay - how much the fog density decays with altitude. 0 = constant density, 0.0049333 = density halves every 500m
         fog_base = 50;                                  // number - meters +/- above Sea Level
         fog_mode = 1;                                   // Fogmode: 0 - apply setFog with param once, nothing else fancy going on.  | 1 - Gets Players Average ASL once and adds that to the fog_base Value. | 2 - Continously adapts fogbase based on player AvgAVL.
         fog_boost = 1;
