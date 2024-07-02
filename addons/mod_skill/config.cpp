dbecf866c877a4fda4a6d0b474c5af3264351082
Mr. Zorn <dennisAzorn@gmail.com>
2024-07-02 23:24:02 +0200

    fixed required Version

--- addons/mod_skill/config.cpp
+++ addons/mod_skill/config.cpp
@@ -7,7 +7,7 @@ class CfgPatches {
 		ADDON_CONFIG_HEADER;		// see based/script_mod.hpp
 
         // Minimum compatible version. When the game's version is lower, pop-up warning will appear when launching the game.
-        requiredVersion = 2.02;
+        requiredVersion = REQUIRED_VERSION;
 
         // Required addons, used for setting load order.
         // When any of the addons is missing, pop-up warning will appear when launching the game.
