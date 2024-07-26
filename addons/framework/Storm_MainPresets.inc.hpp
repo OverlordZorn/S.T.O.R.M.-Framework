5c1799dabc5c4060cedfe45741844b152e8a5d38
Mr. Zorn <dennisAzorn@gmail.com>
2024-07-26 10:58:52 +0200

    Update Storm_MainPresets.inc.hpp

--- addons/framework/Storm_MainPresets.inc.hpp
+++ addons/framework/Storm_MainPresets.inc.hpp
@@ -34,7 +34,7 @@ class PVAR(mainPresets)
         fx_weather_preset = QEGVAR(fx_weather,Sandstorm);
         fx_weather_coef = 1;
 
-        fx_weather_fog_preset = QEGVAR(fx_weather,fog_STATIC_80);
+        fx_weather_fog_preset = QEGVAR(fx_weather,fog_STATIC_90);
         fx_weather_fog_coef = 1;
 
         fx_post_presets[] = {QEGVAR(fx_post,CC_Mars_Storm), QEGVAR(fx_post,FG_Storm), QEGVAR(fx_post,DB_20)};
