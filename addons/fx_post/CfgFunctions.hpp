e9f4b6bf84e1f035ef55356ef0060c31e2036387
OverlordZorn <dennisAzorn@gmail.com>
2025-01-09 13:45:59 +0100

    updated addon structure

---
+++ addons/fx_post/CfgFunctions.hpp
@@ -0,0 +1,16 @@
+class CfgFunctions
+{
+	class ADDON          // Tag
+	{
+		class COMPONENT   // Category
+		{
+			file = PATH_TO_FNC;
+
+			class request {};
+
+			class remote {};
+			class getConfig {};
+			class convertIntensity {};
+		};
+	}; 
+};
--- addons/fx_post/script_component.hpp
+++ addons/fx_post/script_component.hpp
@@ -1,12 +1,5 @@
 #define COMPONENT fx_post
 #define COMPONENT_BEAUTIFIED Post Processing Effects Module
 
+#include "\zrn\storm\addons\main\script_mod.hpp"
 #include "\zrn\storm\addons\main\script_macros.hpp"
-
-
-// #define DEBUG_MODE_FULL
-// #define DISABLE_COMPILE_CACHE
-// #define ENABLE_PERFORMANCE_COUNTERS
-
-
-
