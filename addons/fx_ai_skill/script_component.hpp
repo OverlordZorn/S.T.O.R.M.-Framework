1391bf6fac98ecad7d8f977b6e4b0ce5e7837952
OverlordZorn <dennisAzorn@gmail.com>
2025-01-09 16:02:56 +0100

    mod_skill -> fx_ai_skill

--- addons/fx_ai_skill/script_component.hpp
+++ addons/fx_ai_skill/script_component.hpp
@@ -1,5 +1,5 @@
-#define COMPONENT mod_skill
-#define COMPONENT_BEAUTIFIED AI SubSkill Modifier Module
+#define COMPONENT fx_ai_skill
+#define COMPONENT_BEAUTIFIED AI SubSkill Module
 
 #include "\zrn\storm\addons\main\script_mod.hpp"
 #include "\zrn\storm\addons\main\script_macros.hpp"
